4db2354c049883439bb3f00205561305c538d376
ben <ben@ben-nuc>
2016-04-02 22:55:46 -0700

    added a function'do' to attempt a task a number of times.

---
+++ do.py
@@ -0,0 +1,22 @@
+import time
+
+
+def do(func, times, end=None, pause=0):
+    """
+    Does a task a specificed number of times, or until the end
+    condition is met, pausing between each attempt.
+     func: a function that returns a value.
+     times: the max number of times to run the function.
+     end: a function that takes 1 parameter, the return value of func(), and returns true or false.
+     pause: number of seconds to pause between attempts.
+    """
+    if end is None:
+        end = lambda x: False
+
+    for rval, i in ((func(), i) for i in xrange(times)):
+        if end(rval):
+            return rval
+        if i < times - 1:
+            time.sleep(pause)
+
+    return None
